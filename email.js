821d25a4793fe96aa318831238f44aa6f3ee2f61
TylerJac <sans.gaming235@gmail.com>
2024-06-20 15:44:17 -0400

    added to html and created a js file and changed the name of the css file

---
+++ email.js
@@ -0,0 +1,14 @@
+// Initialize EmailJS with your user ID
+emailjs.init("3xe7QvERMbo26l7sp");
+
+// Function to send email
+function sendEmail(event) {
+    event.preventDefault();
+    
+    emailjs.sendForm('service_krl1hyi', 'template_cd623zy', '#contact-form')
+        .then(function(response) {
+            alert('SUCCESS!', response.status, response.text);
+        }, function(error) {
+            alert('FAILED...', error);
+        });
+}
